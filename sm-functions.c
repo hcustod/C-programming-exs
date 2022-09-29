a6234989b692ad69bb4deadafdf7b3d950b0e4b7
hcustod <111096105+hcustod@users.noreply.github.com>
2022-09-28 21:58:33 -0400

    10-4

---
+++ sm-functions.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void vegas(void); //Prototyping
+
+int main()
+{
+    int a;
+
+    a = 365;
+    printf("In the main() function, a=%d\n", a);
+    vegas();
+    printf("In the main() function, a=%d\n", a);
+    return 0;
+}
+
+void vegas(void)  ///Prototyping - If function is to be used before its defined.
+{
+    int a;
+
+    a = -10;
+    printf("In the vegas() function, a=%d\n", a);
+}
