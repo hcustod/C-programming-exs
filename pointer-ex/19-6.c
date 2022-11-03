6643646785096b1e52921f304551f0a1384311ea
hcustod <111096105+hcustod@users.noreply.github.com>
2022-11-02 23:50:13 -0400

    19-6 & 19-7

--- pointer-ex/19-6.c
+++ pointer-ex/19-6.c
@@ -1,4 +1,19 @@
+// using a pointer to display a string
+
+
 #include <stdio.h>
+
+int main()
+{
+    const char *sample = "From whence cometh my help?\n";
+
+    puts(sample);
+    return 0; 
+}
+
+
+
+/*
 
 int main()
 {
@@ -12,3 +27,5 @@ int main()
     }
     return 0;
 }
+
+*/
